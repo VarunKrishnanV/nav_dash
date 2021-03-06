1a522e6f1900a8dabc9d91bf0e48c42e2dad7e5c
VarunKrishnanV <55345156+VarunKrishnanV@users.noreply.github.com>
2021-03-06 23:50:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menu = document.querySelector(".menu");
+var sidebar = document.querySelector(".sidebar");
+
+menu.addEventListener('click', ()=>{
+    sidebar.classList.toggle("show")
+});
+
+console.log("hello");
